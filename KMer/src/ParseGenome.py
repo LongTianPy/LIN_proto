2ffd51a2ac67516aa202614d13f138583ce2c325
LongTianPy <tianlongapp@gmail.com>
2015-12-04 16:25:44 -0500

    Calculateing pi per 5 mer

--- KMer/src/ParseGenome.py
+++ KMer/src/ParseGenome.py
@@ -18,20 +18,39 @@ def LoadGenome(genomefilepath): # Read and load genomes, concatenate into one wh
     genome = [genomefilepath]+[''.join(contigs)]
     return genome
 
-def CountKMer(genomefilepath,Kmers):
-    genome = LoadGenome(genomefilepath)
-    KMer_counts = [0]*len(Kmers) # Used for store K-mer counting records
-    for i in range(len(Kmers)):
-        pattern = '(?=%s)'%Kmers[i]
-        pattern = re.compile(pattern,re.IGNORECASE)
-        KMer_counts[i] = len([m.start() for m in re.finditer(pattern,genome[1])])
-    return genomefilepath, KMer_counts
+def CalcKMerProb(genome,KMers):
+    L = len(genome)
+    KMer_counts_k = [0]*len(KMers) # Used for store K-mer counting records
+    KMer_counts_1k_1 = [0]*len(KMers)
+    KMer_counts_2k = [0]*len(KMers)
+    KMer_counts_2k_1 = [0]*len(KMers)
+    for i in range(len(KMers)):
+        pattern_k = '(?=%s)'%KMers[i]
+        pattern_1k_1 = '(?=%s)'%KMers[i][:-1]
+        pattern_2k = '(?=%s)'%KMers[i][1:]
+        pattern_2k_1 = '(?=%s)'%KMers[i][1:-1]
+        pattern_k = re.compile(pattern_k,re.IGNORECASE)
+        pattern_1k_1 = re.compile(pattern_1k_1,re.IGNORECASE)
+        pattern_2k = re.compile(pattern_2k,re.IGNORECASE)
+        pattern_2k_1 = re.compile(pattern_2k_1,re.IGNORECASE)
+        KMer_counts_k[i] = len([m.start() for m in re.finditer(pattern_k,genome[1])])
+        KMer_counts_1k_1[i] = len([m.start() for m in re.finditer(pattern_1k_1,genome[1])])
+        KMer_counts_2k[i] = len([m.start() for m in re.finditer(pattern_2k,genome[1])])
+        KMer_counts_2k_1[i] = len([m.start() for m in re.finditer(pattern_2k_1,genome[1])])
+    Total_substrings = L-k+1
+    Prob_appearance_k = [float(i)/Total_substrings for i in KMer_counts_k]
+    Prob_appearance_1k_1 = [float(i)/Total_substrings for i in KMer_counts_1k_1]
+    Prob_appearance_2k = [float(i)/Total_substrings for i in KMer_counts_2k]
+    Prob_appearance_2k_1 = [float(i)/Total_substrings for i in KMer_counts_2k_1]
+    expected_appearance_prob = [Prob_appearance_1k_1[i]*Prob_appearance_2k[i]/Prob_appearance_2k_1[i] for i in range(len(Prob_appearance_2k))]
+    pi_k = [(Prob_appearance_k[i]-expected_appearance_prob[i])/expected_appearance_prob[i] if expected_appearance_prob[i] != 0 else 0 for i in range(len(Prob_appearance_k)) ]
+    return pi_k
 
-def KMerFreq(KMer_counts):
+
+def KMerFreq(genomefilepath,k):
+    genomename, KMer_counts, L = CountKMer(genomefilepath,k)
+    Total_substrings = L-k+1
+    Prob_appearance = [float(i)/Total_substrings for i in KMer_counts]
 
 
-if __name__ == '__main__':
-    genomefilepath = sys.argv[1]
-    k = sys.argv[2]
-    KMers = KMerLib(k)
 
