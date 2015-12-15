7e468c587b74117ee3cb85169294b57208db5e8e
LongTianPy <tianlongapp@gmail.com>
2015-12-15 15:18:32 -0500

    Sliding window method implemented

--- KMer/test/ParseGenome.py
+++ KMer/test/ParseGenome.py
@@ -17,7 +17,7 @@ def LoadGenome(genomefilepath): # Read and load genomes, concatenate into one wh
     return genome
 
 def CalcKMerProb(genome,KMers,k):
-    L = len(genome)
+    L = len(genome[1])
     # KMer_counts_k = [0]*len(KMers) # Used for store K-mer counting records
     # KMer_counts_1k_1 = [0]*len(KMers)
     # KMer_counts_2k = [0]*len(KMers)
@@ -62,8 +62,41 @@ def CalcKMerProb(genome,KMers,k):
     return pi_k
 
 def CalcKMerProb_slidingwindow(genome,KMers,k):
-    L = length(genome[1])
-
+    sequence = genome[1]
+    L = length(sequence)
+    # k
+    KMer_counts_k = [0]*len(KMers)
+    for i in range(L-k+1):
+        substring = sequence[i:(i+k)]
+        idx = KMers.index(substring)
+        KMer_counts_k[idx] += 1
+    # KMer from 1 to k-1
+    KMers_1k1 = [i[:-1] for i in KMers]
+    KMer_counts_1k_1 = [0]*len(K_1Mers)
+    # KMer from 2 to k
+    KMers_2k = [i[1:] for i in KMers]
+    KMer_counts_2k = [0]*len(KMers_2k)
+    for i in range(L-k-1+1):
+        substring = sequence[i:(i+k-1)]
+        idx1 = KMers_1k1.index(substring)
+        idx2 = KMers_2k.index(substring)
+        KMer_counts_1k_1[idx1] += 1
+        KMer_counts_2k[idx2] += 1
+    # KMer from 2 to k-1
+    KMers_2k1 = [i[1:-1] for i in KMers]
+    KMer_counts_2k_1 = [0]*len(KMers_2k1)
+    for i in range(L-k-2+1):
+        substring = sequence[i:(i+k-2)]
+        idx = KMers_2k1.index(substring)
+        KMer_counts_2k_1[idx] += 1
+    Total_substrings = L-k+1
+    Prob_appearance_k = [float(i)/Total_substrings for i in KMer_counts_k]
+    Prob_appearance_1k_1 = [float(i)/Total_substrings for i in KMer_counts_1k_1]
+    Prob_appearance_2k = [float(i)/Total_substrings for i in KMer_counts_2k]
+    Prob_appearance_2k_1 = [float(i)/Total_substrings for i in KMer_counts_2k_1]
+    expected_appearance_prob = [Prob_appearance_1k_1[i]*Prob_appearance_2k[i]/Prob_appearance_2k_1[i] if Prob_appearance_2k_1[i] != 0 else 0 for i in range(len(Prob_appearance_2k))]
+    pi_k = [(Prob_appearance_k[i]-expected_appearance_prob[i])/expected_appearance_prob[i] if expected_appearance_prob[i] != 0 else 0 for i in range(len(Prob_appearance_k)) ]
+    return pi_k
 
 
 
