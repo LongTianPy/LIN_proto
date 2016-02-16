1d12632c5fd52fa282f38644f7eeed7965e1cbcd
LongTianPy <tianlongapp@gmail.com>
2016-02-16 11:00:26 -0500

    Memory usage problem still exists. Can't deal with large data, say Psy x P_putida. Method using pandas dataframe data type implemented for the first edition.

--- NewMatrix/Distance_pandas.py
+++ NewMatrix/Distance_pandas.py
@@ -56,7 +56,7 @@ def main(subjectfilepath, queryfilepath):
     pearson_correlation = lambda column1, column2: scipy.stats.pearsonr(column1, column2)[0]
     cosine_similarity = lambda column1, column2: 1-scipy.stats.distance.cosine(column1,column2)
     print "Calculating distance"
-    result = total_frequency.apply(lambda col1: total_mker_profile.apply(lambda col2: cosine_similarity(col1, col2)))
+    result = total_frequency.apply(lambda col1: total_frequency.apply(lambda col2: cosine_similarity(col1, col2)))
     print "... Done."
     print "Writing distance matrix to %s"%queryfilepath
     result.to_csv('%sdistance.csv'%queryfilepath)
