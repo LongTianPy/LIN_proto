f623794a1f1d02afee226323766660e169693837
LongTianPy <tianlongapp@gmail.com>
2016-02-15 19:01:42 -0500

    Memory usage problem still exists. Can't deal with large data, say Psy x P_putida. Method using pandas dataframe data type implemented for the first edition.

--- NewMatrix/Distance_pandas.py
+++ NewMatrix/Distance_pandas.py
@@ -19,4 +19,36 @@ def read_into_dataframe(countfile):
     df = pd.DataFrame(data)
     return df
 
+def isfilepath(filepath):
+    if filepath.endswith('/'):
+        filepath = filepath
+    else:
+        filepath = filepath + '/'
+    return filepath
 
+def KmerCountNew(filepath):
+    """
+    :param filepath:
+    :return: A k-mer counting profile will be generated on the hardisk
+    """
+    cmd = "kpal count -k 12 %s*.fasta %stmp_count"%(filepath, filepath)
+    os.system(cmd)
+
+# MAIN
+def main(subjectfilepath, queryfilepath):
+    queryfilepath = isfilepath(queryfilepath)
+    KmerCountNew(queryfilepath)
+    new_kmer_profile_path = filepath+'tmp_count'
+    original_kmer_profile = read_into_dataframe(subjectfilepath)
+    new_kmer_profile = read_into_dataframe(queryfilepath)
+    total_mker_profile = pd.concat([original_kmer_profile, new_kmer_profile], axis=1)
+    del original_kmer_profile
+    del new_kmer_profile
+    euclidean_distance = lambda column1, column2: pd.np.linalg.norm(column1 - column2)
+    result = total_mker_profile.apply(lambda col1: total_mker_profile.apply(lambda col2:distance(col1, col2)))
+    print result
+
+if __name__ == '__main__':
+    filepath = sys.argv[2]
+    subjectpath = sys.argv[1]
+    main(queryfilepath=filepath,subjectfilepath=subjectpath)
