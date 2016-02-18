a98243367f2bd6ae5fd94dd03c8ba0256f4aeff4
LongTianPy <tianlongapp@gmail.com>
2016-02-17 20:43:30 -0500

    Trying to only calculate the distances/similarities between original genomes and new genomes. With original frequency table generated beforehand and read directly.

--- NewMatrix/Distance_pandas.py
+++ NewMatrix/Distance_pandas.py
@@ -95,8 +95,8 @@ def generate_distance(subjectpath,queryfilepath):
     # result = total_frequency.apply(lambda new_kmer_column: total_frequency.apply(lambda col2: cosine_similarity(new_kmer_column, col2)))
     # result_new2old = original_frequency.apply(lambda column :cosine_similarity(column, new_kmer_column)) # Usually it's one column
     result_new2old = []
-    for i in original_frequency:
-        result_new2old.append(cosine_similarity(i,new_kmer_column))
+    for i in original_frequency.keys():
+        result_new2old.append(cosine_similarity(original_frequency[i],new_kmer_column))
     print result_new2old
     newrow = [new_kmer_name]+result_new2old
     print newrow
