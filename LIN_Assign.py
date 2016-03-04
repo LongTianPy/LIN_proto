e1bc8af55773527a6d716f2e69c9ea35c0ac182d
LongTianPy <tianlongapp@gmail.com>
2016-03-04 12:23:44 -0500

    Use ANIb calculation from PyANI and did small modification to it.

---
+++ LIN_Assign.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+"""
+This script aims to assign LIN to the new genome by reading and parsing the LINs of its top similar
+genome. See the new LIN assignment strategy for more details.
+"""
+
+# IMPORT
+import MySQLdb
+from MySQLdb import Connect
+
+
+# OBJECT
+class getLIN(object):
+    """
+    Read the LIN of the top similar genome.
+    """
+    def __init__(self, genome, Scheme_ID, similarity):
+        self.genome = genome
+        self.Scheme_ID = Scheme_ID
+        db = Connect('localhost', 'root')
+        c = db.cursor()
+        c.execute('use LINdb_test')
+        c.execute("SELECT LabelNum from Scheme WHERE Scheme_ID={0}".format(Scheme_ID))
+        self.label_num = int(c.fetchone()[0])
+        self.similarity = float(similarity)*100
+        self.parse()
+    def parse(self, genome = None, Scheme_ID = None, similarity = None):
+        if not genome:
+            genome = self.genome
+        if not Scheme_ID:
+            Scheme_ID = self.Scheme_ID
+        if not similarity:
+            similarity = self.similarity
+        # Read the LIN of this genome
+        db = Connect('localhost', 'root')
+        c = db.cursor()
+        c.execute('use LINdb_test')
+        c.execute('SELECT LIN.LIN from LIN, Genome where LIN.Genome_ID=Genome.Genome_ID and Genome.FilePath LIKE "%{0}" and LIN.Scheme_ID={1}'.format(genome, Scheme_ID))
+        lin = c.fetchone()[0].split(',')
+        self.LIN = lin
+        # Read the cutoff of this scheme
+        c.execute('SELECT Cutoff from Scheme where Scheme_ID={0}'.format(Scheme_ID))
+        cutoff = c.fetchone()[0].split(',')
+        cutoff = [float(i) for i in cutoff]
+        for i in range(len(cutoff)-1):
+            if cutoff[i]<similarity and similarity <= cutoff[i+1]:
+                idx_to_change = i+1
+            else:
+                continue
+        self.idx_to_change = idx_to_change
+        self.conserved_LIN = lin[:idx_to_change]
+
+class Assign_LIN(object):
+    """ Get the biggest number assigned to the idx_to_change with the same conserved part of LIN
+    """
+    def __init__(self, getLIN_object):
+        self.idx_to_change = getLIN_object.idx_to_change
+        self.conserved_LIN = ','.join(getLIN_object.conserved_LIN)
+        self.label_num = getLIN_object.label_num
+        self.assign()
+    def assign(self, idx_to_change=None, conserved_LIN=None, label_num=None):
+        if not idx_to_change:
+            idx_to_change = self.idx_to_change
+        if not conserved_LIN:
+            conserved_LIN = self.conserved_LIN
+        if not label_num:
+            label_num = self.label_num
+        db = Connect('localhost', 'root')
+        c = db.cursor()
+        c.execute('use LINdb_test')
+        c.execute('SELECT LIN.LIN from LIN WHERE LIN.LIN LIKE "{0}%"'.format(conserved_LIN))
+        tmp = c.fetchall()
+        LINs = [int(i[0].split(',')[idx_to_change]) for i in tmp]
+        num_to_assign = str(max(LINs)+1)
+        if idx_to_change != label_num - 1:
+            tail = ['0'] * (label_num - 1 - idx_to_change)
+            tail = ','.join(tail)
+            new_LIN = conserved_LIN + ',%s,'%num_to_assign + tail
+        else:
+            new_LIN = conserved_LIN + ',%s,'%num_to_assign
+        self.new_LIN = new_LIN
+
+
+
+
+
+
+
+
+
+
+
