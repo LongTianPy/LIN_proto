a9338d7c7477178a723f2e15ad94abed5dff2961
LongTianPy <tianlongapp@gmail.com>
2016-03-07 11:15:34 -0500

    Rewrite script to fit backend.

--- k_mer.py
+++ k_mer.py
@@ -95,7 +95,7 @@ def generate_distance(subjectpath,queryfilepath):
     print "Calculating cosine similarities..."
     # result = total_frequency.apply(lambda new_kmer_column: total_frequency.apply(lambda col2: cosine_similarity(new_kmer_column, col2)))
     # result_new2old = original_frequency.apply(lambda column :cosine_similarity(column, new_kmer_column)) # Usually it's one column
-    result_new2old = [cosine_similarity(new_kmer_column,original_frequency[i]) for i in original_frequency.keys()]
+    result_new2old = [1-cosine_similarity(new_kmer_column,original_frequency[i]) for i in original_frequency.keys()]
     column_names = original_frequency.keys()
     return column_names, result_new2old
     # newrow = [new_kmer_name]+result_new2old
