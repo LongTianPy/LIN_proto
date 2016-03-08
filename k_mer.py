f9a5f8e62d7f86e43aae9b8f8cdcb32b072f699d
LongTianPy <tianlongapp@gmail.com>
2016-03-08 09:38:36 -0500

    Rewrite script to fit backend.

--- k_mer.py
+++ k_mer.py
@@ -100,7 +100,7 @@ def generate_distance(subjectpath,queryfilepath):
     for i in range(len(result_new2old)):
         similarities[original_frequency.keys()[i]] = result_new2old[i]
     original_frequency[new_kmer_name] = new_kmer_column
-    original_frequency.to_csv('~/Desktop/updated_frequency.csv')
+    original_frequency.to_csv('/home/vinatzerlab/Desktop/updated_frequency.csv')
     return similarities.sort(axis=0, ascending=False, kind="mergesort")
     # newrow = [new_kmer_name]+result_new2old
     # print newrow
