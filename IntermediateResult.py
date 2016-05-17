4f5010b19a60e80b4cf566c916ca292ade130ce2
LongTianPy <tianlongapp@gmail.com>
2016-05-17 10:34:08 -0400

    Avoid multiple visit to database

--- IntermediateResult.py
+++ IntermediateResult.py
@@ -10,7 +10,7 @@ from email.mime.text import MIMEText
 
 def write_kmer_result(top10,db_cursor):
     c = db_cursor
-    f = open("tmp.txt","w")
+    f = open("/home/linproject/Workspace/email_content/tmp.txt","w")
     f.write("After analyzing the k-mer profile of your uploaded genome by fragmenting it into 12-mer, the following "
             "records are decided to be further analyzed by calculating the Average Nucleotide Identity (ANI) "
             "with your uploaded genome, which may take a while.\n")
@@ -27,7 +27,7 @@ def send_email(User_ID=2, db_cursor):
     :param User_ID: Should be able to read from front end, default = 2, which is me.
     :return:
     """
-    fp = open("tmp.txt","r")
+    fp = open("/home/linproject/Workspace/email_content/tmp.txt","r")
     msg = MIMEText(fp.read())
     fp.close()
 
@@ -36,12 +36,12 @@ def send_email(User_ID=2, db_cursor):
     c.execute("SELECT LastName, Email from User where User_ID={0}".format(User_ID))
     tmp = c.fetone()
     LastName = tmp[0]
-    you = [tmp[1]]
+    you = tmp[1]
     msg['Subject'] = "Mr. {0}, here's the preliminary result of your recently submission".format(LastName)
-    msg['From'] = me
-    msg['To'] = you
 
     s = smtplib.SMTP('smtp.gmail.com',587)
+    s.ehlo_or_helo_if_needed()
+    s.starttls()
+    s.login(me, '!Lt19881120')
     s.sendmail(me, you, msg.as_string())
-    s.quit()
-
+    s.close()
