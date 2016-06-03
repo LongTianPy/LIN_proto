709666ba113c50614572467136abdaf53ea61ef5
LongTianPy <tianlongapp@gmail.com>
2016-06-02 20:39:26 -0400

    Writing HTML email

--- IntermediateResult.py
+++ IntermediateResult.py
@@ -71,7 +71,7 @@ def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID):
 
     # Get info of the best match
     Genome_ID_best_hit = new_LIN_object.Genome_ID
-    db_cursor.execute("SELECT AtttributeValue.AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
+    db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
                       "IN (1,4,5)".format(Genome_ID_best_hit))
     if len(tmp) == 0:
         Genus_best_hit = Species_best_hit = Strain_best_hit = "N/A"
