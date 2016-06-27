9bed1201c1ca3ddb8196c050f1881024f99ac39e
LongTianPy <tianlongapp@gmail.com>
2016-06-26 23:29:55 -0400

    Writing HTML email

--- db_build.py
+++ db_build.py
@@ -39,8 +39,10 @@ def build_db():
 		'PRIMARY KEY (Submission_ID))')
 	c.execute('CREATE TABLE User (User_ID int NOT NULL AUTO_INCREMENT,LastName varchar(255) NOT NULL,FirstName varchar(255) NOT NULL,Institute varchar(255) NOT NULL,RegistrationDate text NOT NULL,Username varchar(255) NOT NULL,Password text NOT NULL,Email text NOT NULL,PRIMARY KEY (User_ID))')
 	c.execute("CREATE TABLE AttributeValue (AttributeValue_ID int NOT NULL AUTO_INCREMENT,Genome_ID int NOT NULL,Interest_ID int NOT NULL, Attribute_ID int not null,AttributeValue text not null,User_ID int not null,Private boolean not null,PRIMARY KEY (AttributeValue_ID))")
+    	c.execute("CREATE TABLE Description (Description_ID int not null auto_increment, Genome_ID int not null, Description_Item_ID int not null, Description_Value text not null, PRIMARY KEY (Description_ID))")
+	c.execute("CREATE TABLE Description_Items (Description_Item_ID int not null auto_increment, Description_Item_Name text not null, PRIMARY KEY (Description_Item_ID))")
     	### INITIALIZATION
-	c.execute("insert into User (LastName, FirstName, Institute, RegistrationDate, Username, Password, Email) values ('Tian', 'Long', 'Virginia Tech', '2015-5-17', 'longtian', '123456', 'tianlongapp@gmail.com')")
+	#c.execute("insert into User (LastName, FirstName, Institute, RegistrationDate, Username, Password, Email) values ('Tian', 'Long', 'Virginia Tech', '2015-5-17', 'longtian', '123456', 'tianlongapp@gmail.com')")
     	# Attributes
     	# General entries
 	c.execute("INSERT INTO Attribute (AttributeName) values ('Strain')")
@@ -63,6 +65,12 @@ def build_db():
     	c.execute("INSERT INTO Scheme (Cutoff, LabelNum) values ('70,75,80,85,90,95,96,97,98,99.99999', 10)")
     	c.execute("INSERT INTO Scheme (Cutoff, LabelNum) values ('60,70,80,85,90,95,98,99,99.5,99.6,99.7,99.8,99.9,99.91,99.92,99.93,99.94,99.95,99.96,99.97,99.98,99.99,99.999,99.9999', 24)")
     	c.execute("INSERT INTO Scheme (Cutoff, LabelNum) values ('60,70,75,80,85,90,95,98,98.5,99,99.25,99.5,99.75,99.9,99.925,99.95,99.975,99.99,99.999,99.9999', 20)")
+    	# Description Items
+	c.execute("INSERT INTO Description_Items (Description_Item_Name) values ('Genus')")
+	c.execute("INSERT INTO Description_Items (Description_Item_Name) values ('Species')")
+	c.execute("INSERT INTO Description_Items (Description_Item_Name) values ('Nickname')")
+	c.execute("INSERT INTO Description_Items (Description_Item_Name) values ('Comment')")
+	c.execute("INSERT INTO Description_Items (Description_Item_Name) values ('URL')")
     	# Enter one zika genome to start
     	c.execute("INSERT INTO Genome (Interest_ID, Submission_ID, FilePath, GenomeName) values (2,1,'/home/linproject/Workspace/Zika/init/AY632535.fasta','AY632535')")
     	c.execute("INSERT INTO LIN (Genome_ID, Scheme_ID, SubjectGenome, ANI, LIN) values (1,3,'AY632535', 1, '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')")
