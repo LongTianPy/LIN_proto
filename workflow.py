38dfc91b10a200d27210eff7c12d91bef9dbb782
LongTianPy <tianlongapp@gmail.com>
2016-07-31 10:18:15 -0400

    Re-start with Psy

--- workflow.py
+++ workflow.py
@@ -190,7 +190,11 @@ def main(argv=None): # The genome file name we are expecting for is a
     logging.info("Writing k-mer result.")
     IntermediateResult.write_kmer_result(top10=top10,db_cursor=c,User_ID=User_ID)
     logging.info("Sending k-mer result to the user, User_ID {0}.".format(User_ID))
-    IntermediateResult.send_email(file_source="kmer",User_ID=User_ID,db_cursor=c)
+
+    #################################################################################
+    ### IntermediateResult.send_email(file_source="kmer",User_ID=User_ID,db_cursor=c)
+    #################################################################################
+
     # top10_LINs = [ExtractInfo.get_top10_LIN(i,c) for i in top10] # This can be used to send preliminary results
     # print top10_LINs
     # Get their file paths and copy them to the workspace
@@ -226,13 +230,15 @@ def main(argv=None): # The genome file name we are expecting for is a
     c.execute("INSERT INTO LIN (Genome_ID, Scheme_ID, LIN, SubjectGenome, ANI) values ({0}, 3, '{1}', '{2}', {3})"
               .format(new_Genome_ID, new_LIN, top1_Genome_ID, top1_similarity))
     db.commit()
-    url = IntermediateResult.write_result_page(new_Genome_ID=new_Genome_ID,new_LIN_object=new_LIN_object,new_LIN=new_LIN,db_cursor=c, User_ID=User_ID,Interest_ID=Interest_ID_new_genome)
-    IntermediateResult.write_ANI_result(new_Genome_ID=new_Genome_ID,new_LIN_object=new_LIN_object,new_LIN=new_LIN,db_cursor=c,User_ID=User_ID,url=url)
-    IntermediateResult.send_email(file_source="ANI",User_ID=User_ID,db_cursor=c)
-    c.close()
-    db.close()
-    logging.info("Task completed.")
-    logging.info("#####################################")
+    try:
+        url = IntermediateResult.write_result_page(new_Genome_ID=new_Genome_ID,new_LIN_object=new_LIN_object,new_LIN=new_LIN,db_cursor=c, User_ID=User_ID,Interest_ID=Interest_ID_new_genome)
+        IntermediateResult.write_ANI_result(new_Genome_ID=new_Genome_ID,new_LIN_object=new_LIN_object,new_LIN=new_LIN,db_cursor=c,User_ID=User_ID,url=url)
+        IntermediateResult.send_email(file_source="ANI",User_ID=User_ID,db_cursor=c)
+    except:
+        c.close()
+        db.close()
+        logging.info("Task completed.")
+        logging.info("#####################################")
 
 
 
