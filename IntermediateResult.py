6ecdc47b72b725dff197f5a73bde4ac7aab5f6e4
LongTianPy <tianlongapp@gmail.com>
2016-08-10 16:30:29 -0400

    Result page - debug

--- IntermediateResult.py
+++ IntermediateResult.py
@@ -56,12 +56,12 @@ def write_kmer_result(top10,db_cursor,User_ID):
 def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,url):
     logging.basicConfig(level=logging.DEBUG, filename="/home/linproject/Workspace/LIN_log/logfile_{0}".format(User_ID),
                         filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
-    db_cursor.execute("SELECT LIN.SubjectGenome, LIN.ANI FROM LIN,Genome WHERE LIN.Genome_ID=Genome.Genome_ID "
-                      "and Genome.Genome_ID='{0}'".format(new_Genome_ID))
+    db_cursor.execute("SELECT LIN.SubjectGenome, LIN.ANI, LIN.LIN FROM LIN WHERE"
+                      " Genome_ID='{0}'".format(new_Genome_ID))
     tmp = db_cursor.fetchone()
     best_hit = tmp[0]
     ANI_best_hit = str(float(tmp[1])*100)+'%'
-    LIN_best_hit = new_LIN_object.LIN
+    LIN_new_Genome = tmp[2] # This is a string
 
     # Get info of the new submission
     db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
@@ -73,7 +73,7 @@ def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,u
         Genus_new_Genome = tmp[0][0]
         Species_new_Genome = tmp[1][0]
         Strain_new_Genome = tmp[2][0]
-    LIN_new_Genome = new_LIN
+    LIN_new_Genome = LIN_new_Genome.split(',')
 
     # Get info of the best match
     Genome_ID_best_hit = best_hit
@@ -85,7 +85,10 @@ def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,u
         Genus_best_hit = tmp[0][0]
         Species_best_hit = tmp[1][0]
         Strain_best_hit = tmp[2][0]
-    LIN_best_hit = new_LIN_object.LIN  # This is a list already
+    db_cursor.execute("SELECT LIN from LIN where Genome_ID={0}".format(Genome_ID_best_hit))
+    tmp=db_cursor.fetchone()
+    LIN_best_hit = tmp[0] # A string
+    LIN_best_hit = LIN_best_hit.split(',')
 
     # Get the Genome_IDs of all those sharing the same conserved part of LINs
     db_cursor.execute("SELECT Genome_ID, LIN FROM LIN WHERE LIN LIKE '{0}%' AND Genome_ID <> {1} and Genome_ID <> {2}".
@@ -164,12 +167,12 @@ def write_result_page(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,
     for i in range(len(df.keys())):
         df[df.keys()[i]].append(tmp[i])
 
-    db_cursor.execute("SELECT LIN.SubjectGenome, LIN.ANI FROM LIN,Genome WHERE LIN.Genome_ID=Genome.Genome_ID "
-                      "and Genome.Genome_ID='{0}'".format(new_Genome_ID))
+    db_cursor.execute("SELECT SubjectGenome, ANI, LIN FROM LIN WHERE "
+                      "Genome_ID='{0}'".format(new_Genome_ID))
     tmp = db_cursor.fetchone()
     best_hit = tmp[0]
     ANI_best_hit = str(float(tmp[1]) * 100) + '%'
-    LIN_best_hit = new_LIN_object.LIN
+    LIN_new_Genome = tmp[2]
 
     # Get info of the new submission
     db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
@@ -181,9 +184,13 @@ def write_result_page(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,
         Genus_new_Genome = tmp[0][0]
         Species_new_Genome = tmp[1][0]
         Strain_new_Genome = tmp[2][0]
-    LIN_new_Genome = new_LIN.split(',') # A string
+    LIN_new_Genome = LIN_new_Genome.split(',')
     # Get info of the best match
     Genome_ID_best_hit = best_hit
+    db_cursor.execute("SELECT LIN from LIN where Genome_ID={0}".format(Genome_ID_best_hit))
+    tmp = db_cursor.fetchone()
+    LIN_best_hit = tmp[0]  # A string
+    LIN_best_hit = LIN_best_hit.split(',')
     db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
                       "IN (1,2,3)".format(Genome_ID_best_hit))
     if len(tmp) == 0:
@@ -332,7 +339,8 @@ def write_result_page(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,
         f.write("</select>")
         f.write("<input id='description_input_text[{0}]' name='description_input_text[{1}]' type='text' value='' /></div>")
     f.write("<div class='pure-control-group'><button class='pure-button button-secondary' "
-            "type='button' id='MoreOption_original' onclick='javascript:ExtendOption();'>Add an option</button></div>")
+            "type='button' id='MoreOption_original' onclick='javascript:ExtendOption();'>Add an option</button></div><br>")
+    f.write("<div class='pure-control-group'><button type='submit' class='pure-button pure-button-primary'>Submit</button></div>")
     f.write("<?php")
     f.write("session_start();")
     f.write("$_SESSION['LIN'] = {0};".format(LIN_all))
@@ -371,6 +379,6 @@ def send_email(file_source, db_cursor, User_ID):
     s = smtplib.SMTP('smtp.live.com',587)
     s.ehlo_or_helo_if_needed()
     s.starttls()
-    s.login(me, '1988112019881120')
+    s.login(me, 'Tl19881120')
     s.sendmail(me, you, msg.as_string())
     s.quit()
