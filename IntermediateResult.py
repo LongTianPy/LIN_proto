2dcbce81a7b17fc452536468ed71bbe370a6585a
LongTianPy <tianlongapp@gmail.com>
2016-09-07 17:26:29 -0400

    Result page - debug

--- IntermediateResult.py
+++ IntermediateResult.py
@@ -56,46 +56,70 @@ def write_kmer_result(top10,db_cursor,User_ID):
 def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,url):
     logging.basicConfig(level=logging.DEBUG, filename="/home/linproject/Workspace/LIN_log/logfile_{0}".format(User_ID),
                         filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
-    db_cursor.execute("SELECT LIN.SubjectGenome, LIN.ANI, LIN.LIN FROM LIN WHERE"
-                      " Genome_ID='{0}'".format(new_Genome_ID))
-    tmp = db_cursor.fetchone()
-    best_hit = tmp[0]
-    ANI_best_hit = str(float(tmp[1])*100)+'%'
-    LIN_new_Genome = tmp[2] # This is a string
-
-    # Get info of the new submission
-    db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
-                      "IN (1,2,3)".format(new_Genome_ID))
-    tmp=db_cursor.fetchall()
-    if len(tmp) == 0:
-        Genus_new_Genome = Species_new_Genome = Strain_new_Genome = "N/A"
-    else:
-        Genus_new_Genome = tmp[0][0]
-        Species_new_Genome = tmp[1][0]
-        Strain_new_Genome = tmp[2][0]
-    LIN_new_Genome = LIN_new_Genome.split(',')
-
-    # Get info of the best match
-    Genome_ID_best_hit = best_hit
-    db_cursor.execute("SELECT AttributeValue from AttributeValue WHERE Genome_ID={0} AND Attribute_ID "
-                      "IN (1,2,3)".format(Genome_ID_best_hit))
-    if len(tmp) == 0:
-        Genus_best_hit = Species_best_hit = Strain_best_hit = "N/A"
-    else:
-        Genus_best_hit = tmp[0][0]
-        Species_best_hit = tmp[1][0]
-        Strain_best_hit = tmp[2][0]
-    db_cursor.execute("SELECT LIN from LIN where Genome_ID={0}".format(Genome_ID_best_hit))
-    tmp=db_cursor.fetchone()
-    LIN_best_hit = tmp[0] # A string
-    LIN_best_hit = LIN_best_hit.split(',')
-
-    # Get the Genome_IDs of all those sharing the same conserved part of LINs
-    db_cursor.execute("SELECT Genome_ID, LIN FROM LIN WHERE LIN LIKE '{0}%' AND Genome_ID <> {1} and Genome_ID <> {2}".
-                      format(",".join(new_LIN_object.conserved_LIN),new_Genome_ID,Genome_ID_best_hit))
+    # Get Attribute_IDs
+    db_cursor.execute("SELECT Attribute_IDs FROM Interest WHERE Interest_ID={0}".format(Interest_ID))
+    Attribute_IDs = db_cursor.fetchone()[0]
+    # Get AttributeName
+    db_cursor.execute("SELECT AttributeName FROM Attribute WHERE Attribute_ID in ({0})".format(Attribute_IDs))
     tmp = db_cursor.fetchall()
-    Genome_IDs_related_hits = [int(i[0]) for i in tmp]
-    LINs_related_hits = [i[1] for i in tmp]
+    AttributeName = [i[0] for i in tmp]
+    AttributeName_in_table = ['_'.join(i[0].split(' ')) for i in tmp]
+    AttributeName_in_table_w_o_gss = [i for i in AttributeName_in_table if
+                                      i != 'Genus' and i != 'Strain' and i != 'Species']
+    df = {}
+    for i in AttributeName_in_table:
+        df[i] = []
+    keys = df.keys()  # In case the order changes
+    AttributeName_string = ','.join(keys)
+
+    # Let's first get the info of the new submission
+    db_cursor.execute(
+        "SELECT {0} FROM Genome_to_Attribute WHERE Genome_ID={1}".format(AttributeName_string, new_Genome_ID))
+    new_genome_meta = db_cursor.fetchall()[0]
+    for i in range(len(keys)):
+        df[keys[i]].append(new_genome_meta[i])
+
+    # The second row is the best match
+    # But we need the Genome_ID of it
+    db_cursor.execute(
+        "SELECT SubjectGenome,ANI,LIN FROM LIN where Genome_ID={0}".format(new_Genome_ID)
+    )
+    tmp = db_cursor.fetchall()[0]
+    Genome_ID_best_match = tmp[0]
+    ANI = '{0:.5f}'.format(float(tmp[1]) * 100)
+    LIN_new_Genome = tmp[2]  # A string
+    db_cursor.execute(
+        "SELECT LIN FROM LIN WHERE Genome_ID={0}".format(Genome_ID_best_match)
+    )
+    LIN_best_match = db_cursor.fetchone()[0]  # A string
+    db_cursor.execute(
+        "SELECT {0} FROM Genome_to_Attribute WHERE Genome_ID={1}".format(AttributeName_string, Genome_ID_best_match)
+    )
+    best_match_meta = db_cursor.fetchall()[0]
+    for i in range(len(keys)):
+        df[keys[i]].append(best_match_meta[i])
+
+    # And the related hits sharing the same part of LIN
+    conserved_LIN = ','.join(new_LIN_object.conserved_LIN)
+    db_cursor.execute(
+        "SELECT Genome_ID, LIN from LIN where LIN like '{0}%' and Genome_ID NOT in ({1},{2})".format(conserved_LIN,
+                                                                                                     new_Genome_ID,
+                                                                                                     Genome_ID_best_match)
+    )
+    tmp = db_cursor.fetchall()
+    if len(tmp) != 0:
+        related_hits = True
+        Genome_IDs_related_hits = [int(i[0]) for i in tmp]
+        LINs_related_hits = [i[1] for i in tmp]
+        for i in Genome_IDs_related_hits:
+            db_cursor.execute(
+                "SELECT {0} FROM Genome_to_Attribute WHERE Genome_ID={1}".format(AttributeName_string, i)
+            )
+            related_hit_meta = db_cursor.fetchall()[0]
+            for j in range(len(keys)):
+                df[keys[j]].append(related_hit_meta[j])
+    else:
+        related_hits = False
 
     f = open("/home/linproject/Workspace/email_content/ANI.txt","w")
     f.write("<html><head>\n")
@@ -114,17 +138,14 @@ def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,u
             "<th align='left'>K</th><th align='left'>L</th><th align='left'>M</th><th align='left'>N</th><th align='left'>O</th>"
             "<th align='left'>P</th><th align='left'>Q</th><th align='left'>R</th><th align='left'>S</th><th align='left'>T</th>"
             "</tr>\n")
-    f.write("<tr><td>New Submission</td><td>{0}</td><td>{1}</td><td>{2}</td>".format(Genus_new_Genome,
-                                                                                     Species_new_Genome,
-                                                                                     Strain_new_Genome))
-    for lin in LIN_new_Genome:
-        f.write("<td>{0}</td>".format(lin))
+    f.write("<tr><td>New Submission</td><td>{0}</td><td>{1}</td><td>{2}</td>".format(df["Genus"][0],df["Species"][0],df["Strain"][0]))
+    for each_single_LIN in LIN_new_Genome:
+        f.write("<td>{0}</td>\n".format(each_single_LIN))
     f.write("</tr>\n")
-    f.write("<tr><td>Best match</td><td>{0}</td><td>{1}</td><td>{2}</td>".format(Genus_best_hit,
-                                                                                 Species_best_hit,
-                                                                                 Strain_best_hit))
-    for lin in LIN_best_hit:
-        f.write("<td>{0}</td>".format(lin))
+
+    f.write("<tr><td>Best match</td><td>{0}</td><td>{1}</td><td>{2}</td>".format(df["Genus"][1],df["Species"][2],df["Strain"][3]))
+    for each_single_LIN in LIN_best_match:
+        f.write("<td>{0}</td>\n".format(each_single_LIN))
     f.write("</tr>\n")
     # Write the related hits, who share the conserved LINs
     for i in range(len(Genome_IDs_related_hits)):
@@ -143,6 +164,19 @@ def write_ANI_result(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,u
         for lin in LINs_related_hits[i].split(","):
             f.write("<td>{0}</td>".format(lin))
         f.write("</tr>\n")
+
+    if related_hits:
+        f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23'>Related record(s)</td></tr>")
+        for i in range(len(df["Genus"])-2):
+            real_i = i+2
+            f.write(
+                "<tr><td>Similar record</td>"
+                "<td>{0}</td>"
+                "<td>{1}</td>"
+                "<td>{2}</td>".format(df['Genus'][real_i], df['Species'][real_i],df['Strain'][real_i]))
+            for each_single_LIN in LINs_related_hits[i].split(','):
+                f.write("<td>{0}</td>".format(each_single_LIN))
+            f.write("</tr>")
     f.write("</table>")
     f.write("<p>You can visit the following page to check more details and add descriptions. {0}</p>".format(url))
     f.write("</body></html>")
@@ -314,38 +348,38 @@ def write_result_page(new_Genome_ID, new_LIN_object, new_LIN, db_cursor,User_ID,
             f.write("<p><b>{0}: </b>{1}</p>".format(item, df[each_item][i]))
         f.write("</div>")
     # New Submission
-    f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23'>New Submission</td></tr>")
+    f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23' style='text-align: left; font-weight: bold;'>New Submission</td></tr>")
     f.write("<tr><td style='padding-left: 10px'><input type='checkbox' id='row_checkbox[{0}]' name='row_checkbox[{0}]'"
             "value='{0}'></td>"
-            "<td style='width: 40px; overflow-x: auto;'>{1}</td>"
-            "<td style='width: 40px; overflow-x: auto;'>{2}</td>"
-            "<td style='width: 40px; overflow-x: auto;'>{3}</td>".format(0,df['Genus'][0], df['Species'][0], df['Strain'][0]))
+            "<td style='padding-left: 1px; width: 100px; overflow-x: auto;'>{1}</td>"
+            "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{2}</td>"
+            "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{3}</td>".format(0,df['Genus'][0], df['Species'][0], df['Strain'][0]))
     for each_single_LIN in LIN_new_Genome.split(','):
         f.write("<td class='LIN'>{0}</td>".format(each_single_LIN))
     f.write("</tr>")
     # Best hit
-    f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23'>Best match, ANI: {0}</td></tr>".format(ANI))
+    f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23' style='text-align: left; font-weight: bold;'>Best match, ANI: {0}</td></tr>".format(ANI))
     f.write("<tr><td style='padding-left: 10px'><input type='checkbox' id='row_checkbox[{0}]' name='row_checkbox[{0}]'"
             "value='{0}'></td>"
-            "<td style='width: 40px; overflow-x: auto;'>{1}</td>"
-            "<td style='width: 40px; overflow-x: auto;'>{2}</td>"
-            "<td style='width: 40px; overflow-x: auto;'>{3}</td>".format(1, df['Genus'][1], df['Species'][1],
+            "<td style='padding-left: 1px; width: 100px; overflow-x: auto;'>{1}</td>"
+            "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{2}</td>"
+            "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{3}</td>".format(1, df['Genus'][1], df['Species'][1],
                                                                          df['Strain'][1]))
     for each_single_LIN in LIN_best_match.split(','):
         f.write("<td class='LIN'>{0}</td>".format(each_single_LIN))
     f.write("</tr>")
     # Related hits, if any
     if related_hits:
-        f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23'>Related record(s)</td></tr>")
+        f.write("<tr><td colspan='1'></td><td rowspan='1' colspan='23' style='text-align: left; font-weight: bold;'>Related record(s)</td></tr>")
         for i in range(len(df["Genus"])-2):
             real_i = i+2
             f.write(
                 "<tr><td style='padding-left: 10px'><input type='checkbox' id='row_checkbox[{0}]' name='row_checkbox["
                 "{0}]'"
                 "value='{0}'></td>"
-                "<td style='width: 40px; overflow-x: auto;'>{1}</td>"
-                "<td style='width: 40px; overflow-x: auto;'>{2}</td>"
-                "<td style='width: 40px; overflow-x: auto;'>{3}</td>".format(real_i, df['Genus'][real_i], df['Species'][real_i],
+                "<td style='padding-left: 1px; width: 100px; overflow-x: auto;'>{1}</td>"
+                "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{2}</td>"
+                "<td style='padding-left: 1px; width: 80px; overflow-x: auto;'>{3}</td>".format(real_i, df['Genus'][real_i], df['Species'][real_i],
                                                                              df['Strain'][real_i]))
             for each_single_LIN in LINs_related_hits[i].split(','):
                 f.write("<td class='LIN'>{0}</td>".format(each_single_LIN))
