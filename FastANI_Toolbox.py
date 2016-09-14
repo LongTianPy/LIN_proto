ce9a7018f33392281733750b6093ec1d0bb25460
LongTian <LongTianPy@users.noreply.github.com>
2016-09-14 14:46:19 -0400

    Create FastANI_Toolbox.py
    A different approach of calculating ANI, from scratch

---
+++ FastANI_Toolbox.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# IMPORT
+from Bio import SeqIO
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+
+# FUNCTION
+def split_query_seq(query_seq, frag_size=1020):
+"""
+input: object from concatenated query genome string
+"""
+    fragment = []
+    fragment_append = fragment.append
+    total_size = len(query_seq)
+    for i in range(0, total_size, frag_size):
+        fragment_append(query_seq[i:i+frag_size])
+    return fragment
+    
+def concate_reference_files(top10, cursor, ):
+    concat_ref_file = open('concate_ref.fasta','w')
+    for each_genome_ID in top10:
+        concat_ref_file.write(">{0}\n".format(each_genome_ID))
+        cursor.execute("SELECT FilePath FROM Genome WHERE Genome_ID={0}".format(each_genome_ID))
+        tmp = c.fetchone()[0]
+        each_ref = open(tmp,"r")
+        records = list(SeqIO.parse(each_ref,"fasta"))
+        each_ref.close()
+        seq = ""
+        for record in records:
+            seq_modified = str(record.seq).replace("N","")
+            seq = seq + seq_modified
+        concat_ref_file.write(seq + "\n")
+    concat_ref_file.close()
+
+def makeblastdb():
+    cmd = "makeblastdb "
+
+    
+def run_blastn():
+            
