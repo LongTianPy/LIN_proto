78a7ffdbf3d4c397969c11290f108d15b3ce38ee
LongTianPy <tianlongapp@gmail.com>
2016-12-07 16:22:52 -0500

    LIN tree visualization in D3.js

---
+++ implemented_kmer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+This method is a updated version of k-mer calculation after we realized that strand -- forward and reverse complement
+-- matters in k-mer calculation
+"""
+
+# IMPORT
+
+
+# FUNCTIONS
+
+
+# MAIN
---
+++ mash_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""This script used Mash to determine the subset of genomes to perform ANI calculation
+"""
+
+# IMPORT
+from MySQLdb import Connect
+import pandas as pd
+
+
+# FUNCTIONS
+def
+
+# MAIN
