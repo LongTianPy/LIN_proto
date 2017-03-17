36a9c44a7d1cc17417152f4acc0062d79c742003
LongTianPy <tianlongapp@gmail.com>
2017-03-17 11:27:59 -0400

    Dev - Test how well can we trust mash, or sourmash LIN_assignment included Debug

--- mash_func.py
+++ mash_func.py
@@ -168,8 +168,8 @@ if __name__ == "__main__":
     for each_genome in df.index[1:]:
         if int(df.get_value(each_genome,"Top_Mash")) != int(df.get_value(each_genome,"SubjectGenome")) \
                 and df.get_value(each_genome,"New_G_LINgroup"):
-            print each_genome, df.get_value(each_genome,"Top_Mash")
-            new_LIN = assign_LIN_based_on_mash(each_genome,df.get_value(each_genome,"Top_Mash"),c,conn)
+            print int(each_genome, df.get_value(each_genome,"Top_Mash"))
+            new_LIN = assign_LIN_based_on_mash(each_genome,int(df.get_value(each_genome,"Top_Mash")),c,conn)
         else:
             new_LIN = df.get_value(each_genome,"Assigned_LIN")
         mash_based_LIN.append(new_LIN)
