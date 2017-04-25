e421f018de10e2e98f39ace175709c4196dd24b6
LongTianPy <tianlongapp@gmail.com>
2017-04-25 13:39:18 -0400

    Dev - Test how well can we trust mash, or sourmash LIN_assignment included Debug

--- Tools/export_db.py
+++ Tools/export_db.py
@@ -21,7 +21,23 @@ def export_table(c):
     SubjectGenome = [int(i[1]) for i in tmp]
     ANI = [i[2] for i in tmp]
     LIN = [i[3] for i in tmp]
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=1")
+    tmp = c.fetchall()
+    Genus = [i[0] for i in tmp]
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=2")
+    tmp = c.fetchall()
+    Species = [i[0] for i in tmp]
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=3")
+    tmp = c.fetchall()
+    Subspecies = [i[0] for i in tmp]
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=4")
+    tmp = c.fetchall()
+    Strain = [i[0] for i in tmp]
     df = pd.DataFrame({"SubjectGenome":SubjectGenome,"ANI":ANI,"LIN":LIN},index=Genome_ID)
+    df["Genus"] = Genus
+    df["Species"] = Species
+    df["Subspecies"] = Subspecies
+    df["Strain"] = Strain
     return df
 
 def main(db):
