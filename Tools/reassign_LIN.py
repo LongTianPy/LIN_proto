bd4f5a47d16c39cc5b2273f3869dfbff672b4565
LongTianPy <tianlongapp@gmail.com>
2017-04-27 13:54:20 -0400

    Reassign LIN based on new scheme

---
+++ Tools/reassign_LIN.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+"""This script functions to assign new LINs in a different scheme based on LIN.SubjectGenome and LIN.ANI
+    Remember to keep the LIN table of the new database only first row, or not. You don't have to.
+"""
+
+# IMPORT
+from MySQLdb import Connect
+import pandas as pd
+import LIN_Assign
+
+# FUNCTIONS
+def connect_to_db(db):
+    conn = Connect("localhost","root")
+    c = conn.cursor()
+    c.execute("use {0}".format(db))
+    return conn, c
+
+def fetch_data(old):
+    conn, c = connect_to_db(old)
+    c.execute("select Genome_ID, SubjectGenome, ANI from LIN where Genome_ID>1")
+    tmp = c.fetchall()
+    Genome_ID = [int(i[0]) for i in tmp[1:]]
+    SubjectGenome = [int(i[1]) for i in tmp[1:]]
+    ANI = [i[2] for i in tmp[1:]]
+    df = pd.DataFrame({"SubjectGenome":SubjectGenome,"ANI":ANI},index=Genome_ID)
+    return df
+
+def reassign(new,df):
+    conn, c = connect_to_db(new)
+    Genome_ID = df.index
+    sql = "insert into LIN (Genome_ID, Scheme_ID, SubjectGenome, ANI, LIN) value ({0},4,{1},{2},'{3}')"
+    for each_genome in Genome_ID:
+        if each_genome == 1:
+            c.execute(sql.format(1,1,1,'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'))
+            conn.commit()
+        else:
+            subjectgenome = df.get_value(each_genome,"SubjectGenome")
+            ani = df.get_value(each_genome,"ANI")
+            new_getLIN_object = LIN_Assign.getLIN(Genome_ID=subjectgenome,Scheme_ID=4,similarity=ani)
+            new_LIN =  LIN_Assign.Assign_LIN(getLIN_object=new_getLIN_object,c=c,current_genome=each_genome).new_LIN
+            c.execute(sql.format(each_genome,subjectgenome,ani,new_LIN))
+            conn.commit()
+
+
+# MAIN
+if __name__ == '__main__':
+    df = fetch_data("LINdb")
+    reassign("LINdb",df)
