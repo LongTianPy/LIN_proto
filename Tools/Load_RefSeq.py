0f5953d0218209ca4fe8bcd9196f145380936a5e
LongTianPy <tianlongapp@gmail.com>
2017-05-25 16:51:08 -0400

    Implement mash indexing into the whole workflow

--- Tools/Load_RefSeq.py
+++ Tools/Load_RefSeq.py
@@ -16,7 +16,11 @@ def connect_to_db():
     c.execute("use LINdb_RefSeq")
     return conn, c
 
-def check_identical(new_file,file_paths,c):
+def check_identical(new_file,c):
+    c.execute("select Genome_ID, FilePath from Genome")
+    tmp = c.fetchall()
+    Genome_ID = [int(i[0]) for i in tmp]
+    file_paths = [i[1] for i in tmp]
     for i in range(len(file_paths)):
         if not filecmp.cmp(new_file,file_paths[i]):
             duplicated_genome = i
@@ -30,29 +34,24 @@ def check_identical(new_file,file_paths,c):
 
 
 def load_refseq():
-    refseq_path = "/home/linproject/NCBI_refseq/"
-    meta_data = "/home/linproject/NCBI_refseq/assembly_summary.txt"
-    genome_path = "/home/linproject/NCBI_refseq/GbBac/"
+    refseq_path = "/home/linproject/NCBI/"
+    meta_data = "/home/linproject/NCBI/assembly_summary_type.txt"
+    genome_path = "/home/linproject/NCBI_refseq/Genomes/"
     working_dir = "/home/linproject/Workspace/Psy_166/"
     conn, c = connect_to_db()
-    c.execute("select Genome_ID, FilePath from Genome")
-    tmp = c.fetchall()
-    Genome_ID = [int(i[0]) for i in tmp]
-    FilePath = [i[1] for i in tmp]
     refseq_genomes = [join(genome_path,f) for f in listdir(genome_path) if isfile(join(genome_path,f)) and f.endswith("fna")]
     f = open(meta_data,"r")
     meta = [i.strip().split("\t") for i in f.readlines()[2:]]
     f.close()
     accession = [i[0] for i in meta]
     type = []
-    ftp = []
+    filename = []
     for i in meta:
-        if "type" not in i[-1]:
-            type.append("No")
-            ftp.append(i[-1].split("/")[-1])
+        if i[-2].startswith("ftp"):
+            url = i[-2].split("/")[-1] + "_genomic.fna"
         else:
-            type.append("Yes")
-            ftp.append(i[-2].split("/")[-1])
+            url = i[-3].split("/")[-1] + "_genomic.fna"
+        filename.append(join(genome_path,url))
     organism_name = [i[7] for i in meta]
     infra = [i[8].split("=")[-1] for i in meta]
     strain = []
@@ -65,13 +64,29 @@ def load_refseq():
             strain.append(i)
             resource.append("N/A")
     asm_name = [i[15] for i in meta]
-    filename = [join(genome_path,i+"_genomic.fna") for i in ftp]
+    # filename = [join(genome_path,i+"_genomic.fna") for i in ftp]
+
+
     for i in range(len(accession)):
         if filename[i] in refseq_genomes:
             duplicated_genome = check_identical(filename[i],FilePath)
             if not duplicated_genome:
                 shutil.copy(filename[i],working_dir)
-                cmd = "python /home/linproject/Projects/LIN_proto/workflow.py -i {0}".format(filename[i].split("/")[-1])
+                cmd = "python /home/linproject/Projects/LIN_proto/workflow.py -i {0} -u 2 -s 6 ".format(filename[i].split("/")[-1])
+
+    for i in range(len(filename)):
+        filepath = filename[i]
+        duplicated_genome = check_identical(filepath,c)
+        if not duplicated_genome:
+            shutil.copy(filepath,working_dir)
+            if organism_name[i][0].isupper():
+                if "(" in organism_name[i]:
+                    organism = organism_name[i].split("(")[0]
+                else:
+                    organism = organism_name[i]
+                if organism.startswith("Candidatus"):
+
+
 
 
 
