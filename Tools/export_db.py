5ec383fc0ac0fdb55e3b609e4d043ef362ddab0f
LongTianPy <tianlongapp@gmail.com>
2017-06-12 14:13:45 -0400

    Added get_housekeeping_seq.py for extracting house keeping gene sequences across database

--- Tools/export_db.py
+++ Tools/export_db.py
@@ -15,22 +15,22 @@ def connect_to_db(db):
     return c
 
 def export_table(c):
-    c.execute("select Genome_ID,SubjectGenome,ANI,LIN from LIN")
+    c.execute("select Genome_ID,SubjectGenome,ANI,LIN from LIN WHERE Scheme_ID=4")
     tmp = c.fetchall()
     Genome_ID = [int(i[0]) for i in tmp]
     SubjectGenome = [int(i[1]) for i in tmp]
     ANI = [i[2] for i in tmp]
     LIN = [i[3] for i in tmp]
-    c.execute("select AttributeValue from AttributeValue where Attribute_ID=1")
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=1 AND Genome_ID in ({0})".format(",".join(Genome_ID)))
     tmp = c.fetchall()
     Genus = [i[0] for i in tmp]
-    c.execute("select AttributeValue from AttributeValue where Attribute_ID=2")
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=2 AND Genome_ID in ({0})".format(",".join(Genome_ID)))
     tmp = c.fetchall()
     Species = [i[0] for i in tmp]
     # c.execute("select AttributeValue from AttributeValue where Attribute_ID=3")
     # tmp = c.fetchall()
     # Subspecies = [i[0] for i in tmp]
-    c.execute("select AttributeValue from AttributeValue where Attribute_ID=4")
+    c.execute("select AttributeValue from AttributeValue where Attribute_ID=4 AND Genome_ID in ({0})".format(",".join(Genome_ID)))
     tmp = c.fetchall()
     Strain = [i[0] for i in tmp]
     df = pd.DataFrame({"SubjectGenome":SubjectGenome,"ANI":ANI,"LIN":LIN},index=Genome_ID)
