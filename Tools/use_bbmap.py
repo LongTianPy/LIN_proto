237f2a8ac98b2616484057ca3ad33ce88f952611
LongTianPy <tianlongapp@gmail.com>
2017-07-14 15:38:59 -0400

    change use_bbmap.py

--- Tools/use_bbmap.py
+++ Tools/use_bbmap.py
@@ -6,19 +6,22 @@
 import os
 from os import listdir
 from os.path import isfile,isdir,join
+import sys
 
 # FUNCTIONS
-working_dir = "/home/linproject/Workspace_playaround/bbmap_sketches"
-genome_dir = "/home/linproject/Workspace/Psy_166"
+#working_dir = "/home/linproject/Workspace_playaround/bbmap_sketches"
+#genome_dir = "/home/linproject/Workspace/Psy_166"
 
-def create_sketches():
-    cmd = "sh /home/linproject/Projects/bbmap/sketch.sh in={0} out={1}"
+def create_sketches(working_dir, genome_dir):
+    cmd = "sh sketch.sh in={0} out={1}"
     genomes = [join(genome_dir,f) for f in listdir(genome_dir) if isfile(join(genome_dir,f))]
     genomes_nopath = [f for f in listdir(genome_dir) if isfile(join(genome_dir,f))]
     for i in range(len(genomes)):
-        os.system(cmd.format(genomes[i], genomes_nopath[i]+".sketch"))
+        os.system(cmd.format(genomes[i], join(working_dir,genomes_nopath[i]+".sketch")))
 
 
 # MAIN
 if __name__ == '__main__':
-    create_sketches()
+    working_dir = sys.argv[1]
+    genome_dir = sys.argv[2]
+    create_sketches(working_dir=working_dir, genome_dir=genome_dir)
