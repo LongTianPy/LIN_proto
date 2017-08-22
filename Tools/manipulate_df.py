3b660b45c19e76bf054d3190e421aabaece4d890
LongTianPy <tianlongapp@gmail.com>
2017-08-22 12:57:55 -0400

    developing ani calculation for mpi Fix: compress intermediate files

--- Tools/manipulate_df.py
+++ Tools/manipulate_df.py
@@ -33,9 +33,9 @@ def create_empty_dfs(working_dir):
     files = [file for file in listdir(working_dir) if file.endswith(".fasta")]
     cols = [str(file.split(".")[0]) for file in files]
     idxs = [int(i) for i in cols]
-    ani = pd.DataFrame(columns=cols,index=idxs)
-    cov = pd.DataFrame(columns=cols,index=idxs)
-    aln = pd.DataFrame(columns=cols,index=idxs)
+    ani = pd.DataFrame(columns=cols,index=idxs).to_sparse()
+    cov = pd.DataFrame(columns=cols,index=idxs).to_sparse()
+    aln = pd.DataFrame(columns=cols,index=idxs).to_sparse()
     return ani, cov, aln
 
 def fill_dfs(job_pair,ani,cov,aln):
@@ -45,16 +45,15 @@ def fill_dfs(job_pair,ani,cov,aln):
     file1 = job_pair[2]
     file2 = job_pair[3]
     ani_df = pd.read_table(dir+"/output/ANIblastall_percentage_identity.tab",header=0,index_col=0)
-    cov_df = pd.read_table(dir+"/output/ANIblastall_alignment_coverage.tab",header=0,index_col=0)
-    aln_df = pd.read_table(dir+"/output/ANIblastall_alignment_lengths.tab",header=0,index_col=0)
+    #cov_df = pd.read_table(dir+"/output/ANIblastall_alignment_coverage.tab",header=0,index_col=0)
+    #aln_df = pd.read_table(dir+"/output/ANIblastall_alignment_lengths.tab",header=0,index_col=0)
     ani.loc[int(file1),file2] = ani_df.get_value(int(file1),file2)
     ani.loc[int(file2),file1] = ani_df.get_value(int(file2),file1)
-    cov.loc[int(file1), file2] = cov_df.get_value(int(file1), file2)
-    cov.loc[int(file2), file1] = cov_df.get_value(int(file2), file1)
-    aln.loc[int(file1), file2] = aln_df.get_value(int(file1), file2)
-    aln.loc[int(file2), file1] = aln_df.get_value(int(file2), file1)
+    #cov.loc[int(file1), file2] = cov_df.get_value(int(file1), file2)
+    #cov.loc[int(file2), file1] = cov_df.get_value(int(file2), file1)
+    #aln.loc[int(file1), file2] = aln_df.get_value(int(file1), file2)
+    #aln.loc[int(file2), file1] = aln_df.get_value(int(file2), file1)
     return ani, cov, aln
-
 # MAIN
 if __name__ == '__main__':
     working_dir = sys.argv[1]
@@ -63,6 +62,6 @@ if __name__ == '__main__':
     for each_job in job_map:
         ani, cov, aln = fill_dfs(job_pair=each_job, ani=ani, cov=cov, aln=aln)
     ani.to_csv("../pyani_ANI.csv")
-    cov.to_csv("../pyani_cov.csv")
-    aln.to_csv("../pyani_aln.cov")
+    #cov.to_csv("../pyani_cov.csv")
+    #aln.to_csv("../pyani_aln.cov")
 
