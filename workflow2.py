f4339fd94d8c29b38b87f3b67e95d6433df3ed12
LongTianPy <tianlongapp@gmail.com>
2017-10-24 21:17:20 -0400

    debug line 177

--- workflow2.py
+++ workflow2.py
@@ -168,7 +168,7 @@ def LINgroup_indexing(cursor,metadata,new_genome_filepath):
         top1_coverage = 1
         conserved_LIN = ""
     else:
-        reverse_LIN_dict = {",".join(LIN_table.get_value(each_genome, "LIN")):each_genome for each_genome in metadata.index}
+        reverse_LIN_dict = {metadata.get_value(each_genome, "LIN"):each_genome for each_genome in metadata.index}
         if len(metadata.index) == 1:
             subject_genome_ID = metadata.index[0]
             subject_genome_filepath = metadata.get_value(subject_genome_ID,"FilePath")
@@ -475,6 +475,6 @@ if __name__ == '__main__':
         email_cmd = "python /home/linproject/Projects/LIN_proto/duplicated_upload.py {0} Submission_result {1}".format(
                 user_email,SubjectGenome)
         # os.system(email_cmd)
-    os.system("rm {0}".format(new_genome_filepath))
+    # os.system("rm {0}".format(new_genome_filepath))
     c.close()
     db.close()
