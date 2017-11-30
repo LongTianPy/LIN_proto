420c56042f88b683b9b1ed0174953a3b0bc51bca
LongTianPy <tianlongapp@gmail.com>
2017-11-30 13:42:09 -0500

    extract taxonomy by tax ids

--- get_taxonomy_by_taxid.py
+++ get_taxonomy_by_taxid.py
@@ -32,15 +32,26 @@ def extract_taxonomy(tax_id):
 # MAIN
 if __name__ == '__main__':
     conn,c = connect_to_db()
-    c.execute("select Genome_ID,AttributeValue from AttributeValue WHERE Attribute_ID=15 and AttributeValue<>'N/A'")
+    c.execute("select LIN.Genome_ID,AttributeValue.AttributeValue,LIN.LIN from AttributeValue,LIN WHERE "
+              "LIN.Genome_ID=AttributeValue.Genome_ID Attribute_ID=15 and AttributeValue<>'N/A'")
     tmp = c.fetchall()
     Genome_ID = [int(i[0]) for i in tmp]
     Tax_ID = [str(i[1]) for i in tmp]
+    LIN = [i[2] for i in tmp]
     df = pd.DataFrame()
     df["Taxonomy_ID"] = Tax_ID
+    df["LIN"] = LIN
     df.index = Genome_ID
     full_lineages = []
     full_tax_ids = []
     full_rank = []
     for i in Tax_ID:
+        lineage, tax_id, rank = extract_taxonomy(i)
+        full_lineages.append(lineage)
+        full_tax_ids.append(tax_id)
+        full_rank.append(rank)
+    df["Lineages"] = full_lineages
+    df["Full_Tax_IDs"] = full_tax_ids
+    df["Full_Ranks"] = full_rank
+    df.to_csv("Taxonomy_w_LIN.csv")
 
