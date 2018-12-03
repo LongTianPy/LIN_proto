b689e00912f82976c4bec4aa263ba7f19837c6a0
LongTianPy <tianlongapp@gmail.com>
2018-12-03 14:15:54 -0500

    default value for LINgroup of Genome table is '' (empty string)

--- workflow2.py
+++ workflow2.py
@@ -256,7 +256,7 @@ def load_new_metadata_newversion(c,db,Interest_ID,new_genome,Taxonomy,Attributes
     c.execute("SELECT Submission_ID FROM Submission where User_ID={0} and Time='{1}'".format(User_ID, standardtime))
     Submission_ID = int(c.fetchone()[0])
     os.system("cp {0} {1}".format(tmp_folder + new_genome, original_folder + new_genome))
-    c.execute("INSERT INTO Genome (Interest_ID, Submission_ID, FilePath) VALUES ({0}, {1}, '{2}')"
+    c.execute("INSERT INTO Genome (Interest_ID, Submission_ID, FilePath,LINgroup) VALUES ({0}, {1}, '{2}','')"
               .format(Interest_ID, Submission_ID, original_folder + new_genome))
     db.commit()
     c.execute("SELECT Genome_ID FROM Genome WHERE Submission_ID={0}".format(Submission_ID))
